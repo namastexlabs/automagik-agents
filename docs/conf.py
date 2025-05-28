82d1250b99f3e6dd93bd8ddbadff99d32d251a64
raphaelchristi <raphaelmathuff@hotmail.com>
2025-05-28 09:06:59 -0300

    refactor(docs): remove duplicate exclude_patterns in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -54,11 +54,6 @@ html_static_path = ['_static']
 # Add any paths that contain custom templates here, relative to this directory.
 # templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
 # add_module_names = True
